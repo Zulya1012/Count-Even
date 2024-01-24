7470510e268b409a05212deebd914a75d1590acf
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-24 14:59:59 +0400

    Add files via upload

---
+++ Foreach3.java
@@ -0,0 +1,17 @@
+public class Foreach3{
+	public static void main(String[] arg){
+		int [] m={5, 8, 9, 12, 13, 16};
+		int counteven=0;
+		for(int i:m){
+			if(i%2==0){
+				System.out.println(i);
+				
+				counteven=counteven+1;
+				
+			}
+		}	
+		System.out.println();	
+		System.out.println(counteven);	
+		
+	}
+}
